10cfeb358e70530175d8e1f80aebfe3f99227906
Denis Tadzhibaev <incinerator88@mail.ru>
2023-06-04 16:27:40 +0300

    docs: add allure report and descriptions in test_main_page.py, main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from generator.generator import get_city
 from locators.main_page_locators import MainPageLocators
 from data.urls import main_page_urls as urls
@@ -8,13 +10,17 @@ from pages.base_page import BasePage
 class MainPage(BasePage):
     locators = MainPageLocators()
 
+    @allure.step("Checking current url")
     def check_main_page_current_url(self):
+        """Valid url check"""
         expected_url = urls.get("BASE_URL")
         actual_url = self.driver.current_url
         assert expected_url == actual_url, \
             f"Current url is not equal {expected_url}"
 
+    @allure.step("Checking current title")
     def check_main_page_title(self):
+        """Valid title check"""
         expected_title = dp.get("title")
         actual_title = self.driver.title
         assert expected_title == actual_title, \
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -7,12 +7,16 @@ from pages.main_page import MainPage
 @allure.epic("Testing Main Page")
 class TestMainPage:
 
+    @allure.title("Testing main page current url")
     def test_check_current_url_main_page(self, driver):
+        """This test checks the current site url"""
         page = MainPage(driver, urls["BASE_URL"])
         page.open()
         page.check_main_page_current_url()
 
+    @allure.title("Testing main page current title")
     def test_check_title_main_page(self, driver):
+        """This test checks the actual site title"""
         page = MainPage(driver, urls["BASE_URL"])
         page.open()
         page.check_main_page_title()
